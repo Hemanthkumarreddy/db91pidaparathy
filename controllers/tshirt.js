9a042167fed397f97d473792d8f84fa68301064a
Hemanthkumarreddy <69857171+Hemanthkumarreddy@users.noreply.github.com>
2021-11-11 12:07:04 -0600

    chnages to the tshirt from controllers

--- controllers/tshirt.js
+++ controllers/tshirt.js
@@ -10,6 +10,19 @@ exports.tshirt_list = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
+};
+
+// VIEWS 
+// Handle a show all view 
+exports.tshirt_view_all_Page = async function(req, res) { 
+    try{ 
+        thetshirt = await tshirt.find(); 
+        res.render('tshirt', { title: 'tshirt Search Results', results: thetshirt }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // for a specific tshirt. 
@@ -17,10 +30,26 @@ exports.tshirt_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: tshirt detail: ' + req.params.id); 
 }; 
  
-// Handle tshirt create on POST. 
-exports.tshirt_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: tshirt create POST'); 
-}; 
+// Handle Costume create on POST. 
+exports.tshirt_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new tshirt(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.color = req.body.color; 
+    document.brand = req.body.brand; 
+    document.price = req.body.price; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+};
  
 // Handle tshirt delete form on DELETE. 
 exports.tshirt_delete = function(req, res) { 
--- routes/resource.js
+++ routes/resource.js
@@ -4,28 +4,32 @@ var router = express.Router();
 // Require controller modules. 
 var api_controller = require('../controllers/api'); 
 var tshirt_controller = require('../controllers/tshirt'); 
+const tshirt_controlers= require('../controllers/tshirt');
+
+router.get('/', tshirt_controlers.tshirt_view_all_Page ); 
  
 /// API ROUTE /// 
  
 // GET resources base. 
 router.get('/', api_controller.api); 
  
-/// COSTUME ROUTES /// 
+/// tshirt ROUTES /// 
  
-// POST request for creating a Costume.  
+// POST request for creating a tshirt.  
 router.post('/tshirt', tshirt_controller.tshirt_create_post); 
  
-// DELETE request to delete Costume. 
+// DELETE request to delete tshirt. 
 router.delete('/tshirt/:id', tshirt_controller.tshirt_delete); 
  
-// PUT request to update Costume. 
+// PUT request to update tshirt. 
 router.put('/tshirt/:id', 
 tshirt_controller.tshirt_update_put); 
  
-// GET request for one Costume. 
+// GET request for one tshirt. 
 router.get('/tshirt/:id', tshirt_controller.tshirt_detail); 
  
-// GET request for list of all Costume items. 
+// GET request for list of all tshirt items. 
 router.get('/tshirt', tshirt_controller.tshirt_list); 
+
  
 module.exports = router;
