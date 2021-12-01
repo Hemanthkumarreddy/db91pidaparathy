8af50fd08952c31ad40f05281595fb95def7844c
Hemanthkumarreddy <69857171+Hemanthkumarreddy@users.noreply.github.com>
2021-12-01 15:07:50 -0600

    authentication failure

--- routes/tshirt.js
+++ routes/tshirt.js
@@ -15,10 +15,10 @@ router.get('/detail',tshirt_controlers.tshirt_view_one_Page);
 /* GET create tshirt page */ 
 router.get('/create', tshirt_controlers.tshirt_create_Page);
 
-/* GET create update page */ 
-router.get('/update', tshirt_controlers.tshirt_update_Page);
+/* GET update tshirt page */ 
+router.get('/update', tshirt_controlers.tshirt_update_Page); 
 
-/* GET create costume page */ 
+/* GET delete tshirt page */ 
 router.get('/delete', tshirt_controlers.tshirt_delete_Page); 
 
 module.exports = router;
